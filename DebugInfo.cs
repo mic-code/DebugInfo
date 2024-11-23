a27f6d04c9fa38d40e5e67dfcd43bfb36143192d
Michael Yuen <michael@overimagined.com>
2024-11-23 17:37:45 +0800

    Initial commit

---
+++ DebugInfo.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Overimagined.Common;
+using System.Threading;
+
+public class DebugInfo : SingletonMonoManager<DebugInfo>
+{
+    static Vector3 nPos = new Vector3(float.MinValue, float.MinValue, float.MinValue);
+    const float flashDuration = 0.3f;
+
+    public Color defaultColor = Color.white;
+    public bool disableInEditor;
+    public Vector2 position;
+
+    DebugInfoBuilder builder;
+
+    protected Dictionary<string, Info> infos;
+    HashSet<string> overlayInfos;
+    HashSet<string> activeKeys;
+    List<string> inactiveKeys;
+
+    List<SetData> pendingSets1, pendingSets2;
+    bool pendingFlip;
+
+    Thread mainThread;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        infos = new Dictionary<string, Info>();
+        overlayInfos = new HashSet<string>();
+        activeKeys = new HashSet<string>();
+        inactiveKeys = new List<string>();
+        pendingSets1 = new List<SetData>();
+        pendingSets2 = new List<SetData>();
+
+        builder = new DebugInfoBuilder();
+
+        mainThread = Thread.CurrentThread;
+    }
+
+    public static void SetLeftPadding(int pad)
+    {
+        Instance.builder.vertLayout.padding.left = pad;
+    }
+
+    public static void SetTopPadding(int pad)
+    {
+        Instance.builder.vertLayout.padding.left = pad;
+    }
+
+    public static void Set(object key, Color color)
+    {
+        Set(key.ToString(), null, color, nPos);
+    }
+
+    public static void Set(string key, Color color)
+    {
+        Set(key, null, color, nPos);
+    }
+
+    public static void Set(object key, object content)
+    {
+        Set(key.ToString(), content, Instance.defaultColor);
+    }
+
+    public static void Set(string key, object content)
+    {
+        Set(key, content, Instance.defaultColor);
+    }
+
+    public static void Set(object key, object content, Color color)
+    {
+        Set(key.ToString(), content, color);
+    }
+
+    public static void Set(string key, object content, Color color)
+    {
+        Set(key, content, color, nPos);
+    }
+
+    public static void Set(string key, float content, int significantDigit, int placeHolder = 10)
+    {
+        Set(key, string.Format($"{{0,{placeHolder}:N{significantDigit}}}", content), Instance.defaultColor, nPos);
+    }
+
+    public static void Set(string key, double content, int significantDigit, int placeHolder = 10)
+    {
+        Set(key, string.Format($"{{0,{placeHolder}:N{significantDigit}}}", content), Instance.defaultColor, nPos);
+    }
+
+    public static void Set(string key, object content, Vector3 worldPosition)
+    {
+        Set(key, content, Instance.defaultColor, worldPosition);
+    }
+
+    public static void Set(string key, object content, Color color, Vector3 worldPosition)
+    {
+        var s = content == null ? "null" : content.ToString();
+
+        if (Thread.CurrentThread != Instance.mainThread)
+        {
+            QueueSet(key, content, color, worldPosition);
+            return;
+        }
+
+        Info info;
+        var isOverlay = worldPosition != nPos;
+
+
+        if (Instance.infos.ContainsKey(key))
+        {
+            info = Instance.infos[key];
+            if (!isOverlay && Instance.overlayInfos.Contains(key))
+                Instance.overlayInfos.Remove(key);
+
+            if (info.ValueLabel.text == s)
+                return;
+        }
+        else
+        {
+            info = Instance.builder.AddPair<Info>(key, s, 16, isOverlay);
+            if (isOverlay)
+                Instance.overlayInfos.Add(key);
+        }
+
+        info.ValueLabel.text = s;
+        info.KeyLabel.color = color;
+        info.worldPosition = worldPosition;
+
+        if (!Instance.activeKeys.Contains(key))
+            Instance.activeKeys.Add(key);
+
+
+        info.lastUpdated = Time.time;
+        Instance.infos[key] = info;
+    }
+
+    public static void QueueSet(string key, object content, Color color, Vector3 worldPosition)
+    {
+        var list = Instance.pendingFlip ? Instance.pendingSets1 : Instance.pendingSets2;
+        list?.Add(new SetData { key = key, content = content, color = color, worldPosition = worldPosition });
+    }
+
+    public static void RemoveText(string name)
+    {
+        if (Instance.infos.ContainsKey(name))
+            Instance.infos.Remove(name);
+    }
+
+    public static void Clear(string key)
+    {
+        if (Instance.infos.ContainsKey(key))
+        {
+            Object.Destroy(Instance.infos[key].KeyLabel.transform.parent.gameObject);
+            Instance.infos.Remove(key);
+            Instance.activeKeys.Remove(key);
+            Instance.inactiveKeys.Remove(key);
+            Instance.overlayInfos.Remove(key);
+        }
+    }
+
+    public static void Clear()
+    {
+        foreach (var info in Instance.infos.Values)
+            Object.Destroy(info.KeyLabel.transform.parent.gameObject);
+
+        Instance.infos.Clear();
+    }
+
+    void Update()
+    {
+        var list = Instance.pendingFlip ? Instance.pendingSets2 : Instance.pendingSets1;
+        foreach (var s in list)
+            Set(s.key, s.content, s.color, s.worldPosition);
+        list.Clear();
+        pendingFlip = !pendingFlip;
+
+        inactiveKeys.Clear();
+
+        foreach (var key in activeKeys)
+        {
+            var info = infos[key];
+            var elasped = Time.time - info.lastUpdated;
+            var rgb = info.Background.color;
+            info.Background.color = new Color(rgb.r, rgb.g, rgb.b, Mathf.Clamp(1 - (float)elasped / flashDuration, 0.5f, 1f));
+
+            if (elasped > flashDuration)
+                inactiveKeys.Add(key);
+        }
+
+        foreach (var key in inactiveKeys)
+            activeKeys.Remove(key);
+
+        foreach (var key in overlayInfos)
+        {
+            var info = infos[key];
+            var point = Camera.main.WorldToScreenPoint(info.worldPosition);
+            ((RectTransform)info.KeyLabel.transform.parent).anchoredPosition = point;
+        }
+    }
+
+    void OnDestroy()
+    {
+        builder.Destroy();
+    }
+
+    protected struct Info : IKeyValueUI
+    {
+        public Color color;
+        public double lastUpdated;
+        public Vector3 worldPosition;
+
+        public TMP_Text KeyLabel { get; set; }
+        public TMP_Text ValueLabel { get; set; }
+        public Image Background { get; set; }
+    }
+
+    protected struct SetData
+    {
+        public string key;
+        public object content;
+        public Color color;
+        public Vector3 worldPosition;
+    }
+}
---
+++ DebugInfoBuilder.cs
@@ -0,0 +1,96 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugInfoBuilder
+{
+    public GameObject root;
+    public Canvas defaultCanvas, overlayCanvas;
+    public VerticalLayoutGroup vertLayout;
+
+    public DebugInfoBuilder()
+    {
+        root = new GameObject("DebugCanvas");
+        var defaultHost = new GameObject("Default");
+        var overlayHost = new GameObject("Overlay");
+
+        defaultHost.transform.parent = root.transform;
+        overlayHost.transform.parent = root.transform;
+
+        defaultCanvas = defaultHost.AddComponent<Canvas>();
+        defaultCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        defaultCanvas.sortingOrder = 100;
+        
+        vertLayout = defaultHost.AddComponent<VerticalLayoutGroup>();
+        vertLayout.childForceExpandHeight = false;
+        vertLayout.childControlHeight = false;
+        vertLayout.childControlWidth = false;
+
+        overlayCanvas = overlayHost.AddComponent<Canvas>();
+        overlayCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        overlayCanvas.sortingOrder = 100;
+    }
+
+    public T AddPair<T>(string key, string value, float fontSize, bool isOverlay) where T : IKeyValueUI, new()
+    {
+        var go = new GameObject(key);
+        var rt = go.AddComponent<RectTransform>();
+        rt.transform.SetParent(isOverlay ? overlayCanvas.transform : defaultCanvas.transform, false);
+        rt.sizeDelta = new Vector2(100, 26);
+        rt.pivot = Vector2.zero;
+        var horiLayout = go.AddComponent<HorizontalLayoutGroup>();
+        horiLayout.padding.left = 10;
+        horiLayout.padding.right = 10;
+
+        var contentFitter = go.AddComponent<ContentSizeFitter>();
+        contentFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+
+        var bg = go.AddComponent<Image>();
+        bg.color = new Color(0, 0, 0, 0.5f);
+
+        var keyLabel = AddText(go, key, fontSize, TextAlignmentOptions.Left);
+        keyLabel.name = "Key";
+
+        var separeatorObject = new GameObject("Seperator");
+        separeatorObject.transform.SetParent(go.transform, false);
+        var element = separeatorObject.AddComponent<LayoutElement>();
+        element.minWidth = 10;
+
+        var valueLabel = AddText(go, value, fontSize, TextAlignmentOptions.Right);
+        valueLabel.name = "Value";
+
+        if (isOverlay)
+        {
+            rt.anchorMin = Vector2.zero;
+            rt.anchorMax = Vector2.zero;
+            rt.pivot = Vector2.zero;
+        }
+
+        return new T { KeyLabel = keyLabel, ValueLabel = valueLabel, Background = bg };
+    }
+
+    TextMeshProUGUI AddText(GameObject parent, string text, float fontSize, TextAlignmentOptions alignment)
+    {
+        var go = new GameObject("Text");
+        go.transform.SetParent(parent.transform, false);
+        var tmpText = go.AddComponent<TextMeshProUGUI>();
+        tmpText.text = text;
+        tmpText.fontSize = fontSize;
+        tmpText.enableWordWrapping = false;
+        tmpText.alignment = alignment;
+
+        return tmpText;
+    }
+
+    public void Destroy()
+    {
+        Object.Destroy(root);
+    }
+}
+
+public interface IKeyValueUI
+{
+    Image Background { get; set; }
+    TMP_Text KeyLabel { get; set; }
+    TMP_Text ValueLabel { get; set; }
+}
